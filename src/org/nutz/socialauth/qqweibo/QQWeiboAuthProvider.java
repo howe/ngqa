819e806e656f7a7e115035c708d0454be40b16f6
wendal <wendal1985@gmail.com>
2012-02-11 23:02:31 +0800

    add qqweibo and kaixin001 , not work

---
+++ src/org/nutz/socialauth/qqweibo/QQWeiboAuthProvider.java
@@ -0,0 +1,64 @@
+package org.nutz.socialauth.qqweibo;
+
+import java.util.Map;
+
+import org.brickred.socialauth.Profile;
+import org.brickred.socialauth.exception.ServerDataException;
+import org.brickred.socialauth.exception.SocialAuthException;
+import org.brickred.socialauth.oauthstrategy.OAuth1;
+import org.brickred.socialauth.oauthstrategy.OAuth2;
+import org.brickred.socialauth.util.Constants;
+import org.brickred.socialauth.util.OAuthConfig;
+import org.brickred.socialauth.util.Response;
+import org.nutz.json.Json;
+import org.nutz.socialauth.AbstractOAuthProvider;
+
+/**
+ * 实现QQ帐号登录
+ * 
+ * @author wendal
+ */
+@SuppressWarnings("serial")
+public class QQWeiboAuthProvider extends AbstractOAuthProvider {
+
+	public QQWeiboAuthProvider(final OAuthConfig providerConfig) {
+		super(providerConfig);
+		ENDPOINTS.put(Constants.OAUTH_AUTHORIZATION_URL,"https://open.t.qq.com/cgi-bin/authorize");
+		ENDPOINTS.put(Constants.OAUTH_REQUEST_TOKEN_URL,"https://open.t.qq.com/cgi-bin/request_token");
+		AllPerms = new String[] {};
+		AuthPerms = new String[] {};
+		authenticationStrategy = new OAuth1(config, ENDPOINTS);
+		authenticationStrategy.setPermission(scope);
+		authenticationStrategy.setScope(getScope());
+
+		PROFILE_URL = "http://open.t.qq.com/api/user/info";
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Profile authLogin() throws Exception {
+		String presp;
+
+		try {
+			Response response = authenticationStrategy.executeFeed(PROFILE_URL);
+			presp = response.getResponseBodyAsString(Constants.ENCODING);
+		} catch (Exception e) {
+			throw new SocialAuthException("Error while getting profile from "
+					+ PROFILE_URL, e);
+		}
+		try {
+			//System.out.println("User Profile : " + presp);
+			Map<String, Object> data = Json.fromJson(Map.class, presp);
+			if ("msg".equals(data.get("msg")))
+				throw new SocialAuthException("Error: " + presp);
+			if (userProfile == null)
+				userProfile = new Profile();
+			data = (Map<String, Object>) data.get("data");
+			userProfile.setValidatedId(data.get("openid").toString());
+			return userProfile;
+
+		} catch (Exception ex) {
+			throw new ServerDataException(
+					"Failed to parse the user profile json : " + presp, ex);
+		}
+	}
+}
