15a4cc4804e560c321899d90fe05ab482b3031bb
ywjno <ywjno.dev@gmail.com>
2012-02-17 10:59:02 +0800

    get data form ajax method

--- ROOT/js/index.js
+++ ROOT/js/index.js
@@ -4,11 +4,18 @@ $(function() {
     var questionTagsStr = "<p>Question at {0}</p>";
     var tags = ['question', 'answer'];
     var html = '';
-    $.each([1, 2, 3, 4], function (index, value) {
-        html = String.format(questionInfoStr, 'user_name', '2012-02-13 16:18:10');
-        html += String.format(questionTitleStr, value, "这是一个神奇的网站");
-        html += String.format(questionTagsStr, getTagsHTML(tags));
-        html = String.format('<td class="questioner-img"><img src="{0}" alt="{1}"></td>', './img/img.jpeg', 'UserName') + '<td>' + html + '</td>';
-        $("#questions").append($('<tr>' + html +'</tr>'));
-      });
+
+    $.get('./question/query/list', function(data) {
+        if (data['ok']) {
+            $.each(data, function (key, value) {
+                $.each(value['data']['data'], function (key, value) {
+                    html = String.format(questionInfoStr, 'user_name', '2012-02-13 16:18:10');
+                    html += String.format(questionTitleStr, value, "这是一个神奇的网站");
+                    html += String.format(questionTagsStr, getTagsHTML(tags));
+                    html = String.format('<td class="questioner-img"><img src="{0}" alt="{1}"></td>', './img/img.jpeg', 'UserName') + '<td>' + html + '</td>';
+                    $("#questions").append($('<tr>' + html +'</tr>'));
+                });
+            });
+        }
+    });
 });
