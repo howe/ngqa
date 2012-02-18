c1c2ef482ea1cb8509684206f7d6c8e01f9c23af
wendal <wendal1985@gmail.com>
2012-02-18 16:25:36 +0800

    修正QQ登录

--- src/org/nutz/socialauth/qq/QQAuthProvider.java
+++ src/org/nutz/socialauth/qq/QQAuthProvider.java
@@ -1,7 +1,8 @@
 package org.nutz.socialauth.qq;
 
+import java.util.Map;
+
 import org.brickred.socialauth.Profile;
-import org.brickred.socialauth.exception.ServerDataException;
 import org.brickred.socialauth.exception.SocialAuthException;
 import org.brickred.socialauth.oauthstrategy.OAuth2;
 import org.brickred.socialauth.util.Constants;
@@ -28,34 +29,35 @@ public class QQAuthProvider extends AbstractOAuthProvider {
 		authenticationStrategy.setPermission(scope);
 		authenticationStrategy.setScope(getScope());
 
-		PROFILE_URL = "https://graph.qq.com/user/get_user_info";
+		PROFILE_URL = "https://graph.qq.com/oauth2.0/me";
 	}
 
+	@SuppressWarnings("unchecked")
 	protected Profile authLogin() throws Exception {
 		String presp;
-
 		try {
 			Response response = authenticationStrategy.executeFeed(PROFILE_URL);
-			presp = response.getResponseBodyAsString(Constants.ENCODING);
+			presp = response.getResponseBodyAsString(Constants.ENCODING); // callback( {"client_id":"YOUR_APPID","openid":"YOUR_OPENID"} );
+			if (presp != null) {
+				presp = presp.trim().intern();
+				if (presp.startsWith("callback(") && presp.endsWith(");")) {
+					presp = presp.substring(presp.indexOf("{"), presp.indexOf("}") + 1);
+					Map<String, String> map = (Map<String, String>) Json.fromJson(presp);
+					if (map.get("openid") != null) {
+						Profile p = new Profile();
+						p.setValidatedId(map.get("openid")); // QQ定义的
+						p.setProviderId(getProviderId());
+						userProfile = p;
+						return p;
+					}
+				}
+			}
+			
+			throw new SocialAuthException("QQ Error : " + presp);
 		} catch (Exception e) {
 			throw new SocialAuthException("Error while getting profile from "
 					+ PROFILE_URL, e);
 		}
-		try {
-			System.out.println("User Profile : " + presp);
-			QQUser qqUser = Json.fromJson(QQUser.class, presp);
-			if (qqUser.getRet() != 0)
-				throw new SocialAuthException("QQ Error: " + qqUser.getMsg());
-			Profile p = new Profile();
-			p.setValidatedId(qqUser.getOpenid()); // QQ定义的
-			p.setEmail(qqUser.getEmail());
-			p.setProviderId(getProviderId());
-			userProfile = p;
-			return p;
 
-		} catch (Exception ex) {
-			throw new ServerDataException(
-					"Failed to parse the user profile json : " + presp, ex);
-		}
 	}
 }
