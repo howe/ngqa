3095498db6fb8dcace425c04008af3a34b5fb831
ywjno <ywjno.dev@gmail.com>
2012-03-06 17:27:59 +0800

    add is_login logic

--- ROOT/js/application.js
+++ ROOT/js/application.js
@@ -56,14 +56,25 @@ function loginHTML() {
                         <li><a href="#">tags</a></li>\
                         <li><a href="./ask.jsp">Ask!</a></li>\
                     </ul>\
-                    <ul class="nav pull-right">\
-                        <li><a href="#signin" class="signin">signin</a></li>\
-                    </ul>\
+                    <ul class="nav pull-right" id="signin"></ul>\
                 </div>\
             </div>\
         </div>\
     </div>';
     $("#navbar").append($(navbarTemplate));
+
+    $.get('./me', function (data) {
+        if (data['ok']) {
+            var showName = data['data']['nickName'];
+            if(!showName) {
+                showName = data['data']['id'];
+            }
+            var showHTML = '<p class="navbar-text pull-right">Welcome, {0} <a href="/user/logout" class="signin">Logout</a></p>';
+            $("#signin").append($(String.format(showHTML, showName)));
+        } else {
+            $("#signin").append($('<p class="navbar-text pull-right"><a href="#signin" class="signin">signin</a></p>'));
+        }
+    }, 'json');
 
     var loginTemplate = '<div class="navbar-inner log-width pull-right">\
         {{#link}}\
